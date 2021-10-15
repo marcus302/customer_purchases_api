a9bec32c1b52b876dbcc2984de26eb52a17106d2
Mark de Wijk <dewijkmark@gmail.com>
2021-10-15 13:05:51 +0200

    It's now possible to get a customer's purchases with the customer route and to delete or edit them with the customer purchase route. Ideally I would have used Marshmallow and make everyting REST.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -23,12 +23,20 @@ class CustomerPurchase(db.Model):
     product_id = db.Column(db.Integer, db.ForeignKey("product.id"),
                            nullable=False, primary_key=True)
     quantity = db.Column(db.Integer, nullable=False)
-    # TODO: Remove on cascade.
+    # TODO: Make sure customer purchases are deleted in cascade when a customer or product is removed.
     customer = db.relationship("Customer", backref=db.backref("customer_purchases"))
     product = db.relationship("Product", backref=db.backref("customer_purchases"))
 
     def __repr__(self):
         return f"{self.quantity} pieces of {self.product_id} for {self.customer_id}"
+    
+    # TODO: If I had the time to implement Marshmallow, this would be unnecessary.
+    def to_json(self):
+        return {
+            "customer_id": self.customer_id,
+            "product_id": self.product_id,
+            "quantity": self.quantity
+        }
 
 
 class Product(db.Model):
@@ -39,11 +47,55 @@ class Product(db.Model):
         return self.name
 
 
-@app.route("/customer_purchase/<customer_purchase_id>", methods=["POST", "DELETE"])
-def customer_purchase(customer_purchase_id):
-    return f"<p>Hello, {customer_purchase_id}!</p>"
+db.create_all()
+Customer.query.delete()
+Product.query.delete()
+CustomerPurchase.query.delete()
+db.session.commit()
+c = Customer(first_name="Mark", last_name="de Wijk", address="Bloemsingel 157")
+p = Product(name="Ferrari")
+cp = CustomerPurchase(quantity=5, customer=c, product=p)
+db.session.add(c)
+db.session.add(p)
+db.session.add(cp)
+db.session.commit()
+
+
+@app.route("/customer_purchase/", methods=["POST", "DELETE"])
+def customer_purchase():
+    customer_id = request.args.get("customer_id")
+    product_id = request.args.get("product_id")
+    if request.method == "DELETE":
+        customer_purchase = CustomerPurchase.query.filter_by(
+            customer_id=customer_id,
+            product_id=product_id
+        ).one()
+        db.session.delete(customer_purchase)
+        db.session.commit()
+        return jsonify({"success": True}), 200
+    elif request.method == "POST":
+        quantity = request.args.get("quantity")
+        customer_purchase = CustomerPurchase.query.filter_by(
+            customer_id=customer_id,
+            product_id=product_id
+        ).first()
+        if customer_purchase is not None:
+            # Combination already exists, so update.
+            customer_purchase.quantity = quantity
+            db.session.add(customer_purchase)
+            db.session.commit()
+            return jsonify({"success": True}), 200
+        else:
+            # Combination does not yet exist, so add it.
+            customer = Customer.query.filter_by(id=customer_id).one()
+            product = Product.query.filter_by(id=product_id).one()
+            customer_purchase = CustomerPurchase(quantity=quantity, customer=customer, product=product)
+            db.session.add(customer_purchase)
+            db.session.commit()
+            return jsonify({"success": True}), 200
 
 
 @app.route("/customer/<customer_id>", methods=["GET"])
 def customer(customer_id):
-    return f"<p>Hello, {customer_id}!</p>"
+    customer = Customer.query.filter_by(id=customer_id).one()
+    return jsonify(json_list=[x.to_json() for x in customer.customer_purchases])
