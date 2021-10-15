233bff9993015814766f67bde7f989240172e5e1
Mark de Wijk <dewijkmark@gmail.com>
2021-10-15 10:59:37 +0200

    Defined models for the objects that I need: Customer, CustomerPurchase and Product. Note: These do not have any relationships with each other as of yet. I will do that next.

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    # TODO: Split into street name, house number, city, postal code and country?
+    address = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class CustomerPurchase(db.Model):
+    customer_id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"{self.quantity} pieces of {self.product_id} for {self.customer_id}"
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return self.name
 
 
 @app.route("/customer_purchase/<customer_purchase_id>", methods=["POST", "DELETE"])
